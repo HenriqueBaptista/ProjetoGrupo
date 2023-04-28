9c501ace7138e9787d4dfa167cf8ce4c77617c73
Felipe Batista E Baptista <38279457801@info.local>
2023-04-28 17:10:51 -0300

    criada a classe Credito

---
+++ Credito.cs
@@ -0,0 +1,17 @@
+
+namespace ProjetoGrupo
+{
+    private class Credito
+    {
+        private float Limite()
+        {
+            float limite = Console.WriteLine($"Seu limite é: {}");
+            return limite;
+        }
+        // juros 5% até 6 parcelas
+        if (parcelas)
+        {
+            
+        }
+    }
+}
