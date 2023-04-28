9eee80ed4a80d27e24402b2799fcfc42fd53cb67
Artur Fiorentino <49957976885@info.local>
2023-04-28 17:15:23 -0300

    Criada classe boleto

---
+++ Boleto.cs
@@ -0,0 +1,11 @@
+namespace ProjetoGrupo
+{
+    public class Boleto
+    {
+        public string CodigoDeBarras;
+
+        public void Registrar(){
+            Console.WriteLine($"Sua compra foi efetuada com sucesso!!");
+        }
+    }
+}
