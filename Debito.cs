1700f5af2371c4844729a61b7fc6cbcfe09786bc
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-05-02 15:25:15 -0300

    Debito

---
+++ Debito.cs
@@ -0,0 +1,30 @@
+namespace ProjetoGrupo
+{
+    public class PagamentoDebito : Cartao
+    {
+        private float saldo = 3000;
+        float valor;
+
+        public void Pagar(float saldoAtual)
+        {
+            if (valor > saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+            else if (saldo >= valor)
+            {
+                saldoAtual = saldo - valor;
+                Console.WriteLine($"Pagamento de R${valor} realizado com sucesso. Novo saldo: R${saldoAtual}");
+            }
+            else
+            {
+                Console.WriteLine($"Erro! Tente novamente");
+            }
+        }
+
+        public override void Pagar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
